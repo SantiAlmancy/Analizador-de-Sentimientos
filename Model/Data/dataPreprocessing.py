609c58ff26c5da77399dbce1e5e08ba5f768e533
Sebastian Itamari <sebaton66@gmail.com>
2024-06-09 19:57:02 -0400

    languageFiltering - Filtering language from data

--- Model/Data/dataPreprocessing.py
+++ Model/Data/dataPreprocessing.py
@@ -4,13 +4,32 @@ import ast
 from langdetect import detect
 from langdetect.lang_detect_exception import LangDetectException
 
-pathOriginalData = os.getenv('ORIGINAL_DATA_PATH')
-print(pathOriginalData)
+def detectLanguage(text):
+    try:
+        return detect(text)
+    except LangDetectException:
+        return 'unknown'
+    
+def extractOverall(ratingsText):
+    ratingsDictionary = ast.literal_eval(ratingsText)
+    return ratingsDictionary.get('overall', None)
 
-# Importing the data
-data = pd.read_csv(pathOriginalData)
+if __name__ == "__main__":
+    pathOriginalData = os.getenv('ORIGINAL_DATA_PATH')
 
-# Removing rows with null values
-data.dropna()
+    # Importing the data
+    data = pd.read_csv(pathOriginalData)
 
-print(data)
+    # Adding a column 'overall' with the extracted value from the column 'ratings'.
+    data['overall'] = data['ratings'].apply(extractOverall)
+
+    # Removing rows with null values
+    data.dropna()
+
+    # Adding a column 'language' with the language of the text in the column 'text'
+    data['language'] = data['text'].apply(detectLanguage)
+
+    # Filtering the language of data
+    data = data[data['language'] == 'en']
+
+    print(data)
