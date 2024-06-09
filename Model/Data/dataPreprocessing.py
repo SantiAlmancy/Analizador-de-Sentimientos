aa2776ae737253bfba6b991bac24f0b02ebad917
Sebastian Itamari <sebaton66@gmail.com>
2024-06-09 19:32:17 -0400

    languageFiltering - Importing data and removing null values

--- Model/Data/dataPreprocessing.py
+++ Model/Data/dataPreprocessing.py
@@ -1,3 +1,16 @@
 import os
+import pandas as pd
+import ast
+from langdetect import detect
+from langdetect.lang_detect_exception import LangDetectException
 
-print(os.getenv('ORIGINAL_DATA_PATH'))
+pathOriginalData = os.getenv('ORIGINAL_DATA_PATH')
+print(pathOriginalData)
+
+# Importing the data
+data = pd.read_csv(pathOriginalData)
+
+# Removing rows with null values
+data.dropna()
+
+print(data)
