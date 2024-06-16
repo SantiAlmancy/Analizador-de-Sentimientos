1ac945b428696a983176c4e5bc905ae32a36ee62
Santiago Becerra <santialmancy@gmail.com>
2024-06-16 12:37:23 -0400

    character&SpacesRemoval - Removing special characters

--- Model/Data/StripCharsSpaces.py
+++ Model/Data/StripCharsSpaces.py
@@ -1,4 +1,7 @@
 import re
+import os
+import pandas as pd
+from dotenv import load_dotenv, dotenv_values 
 
 # (\\b[A-Za-z] \\b|\\b [A-Za-z]\\b):
 # - \\b[A-Za-z] \\b: Matches a single alphabetic character [A-Za-z] surrounded by word boundaries \\b (i.e., not preceded or followed by other letters or digits).
@@ -8,3 +11,8 @@ import re
 def singleCharacterRemoval(sentence):
   sentence = re.sub('(\\b[A-Za-z] \\b|\\b [A-Za-z]\\b)', '', sentence)
   return sentence
+
+# Creates a regex pattern to match any character from charsList, treating them literally.
+def specialCharacterRemoval(sentence, charsList):
+   regex_pattern = '[' + re.escape(''.join(charsList)) + ']'
+   return re.sub(regex_pattern, ' ', sentence)
