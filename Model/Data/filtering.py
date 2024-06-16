0cf081caf5963d0d078b38740dc42883e91f212e
Alejandro Diaz Torres <alejandrodiaz1@upb.edu>
2024-06-15 23:40:36 -0400

    Creating method for numbers replacement
    Replacing "one" for '1'

--- Model/Data/filtering.py
+++ Model/Data/filtering.py
@@ -6,3 +6,19 @@ def remove_punctuation(text):
     cleaned_text = re.sub(r'[^\w\s!]', '', text)
     cleaned_text = cleaned_text.strip()
     return cleaned_text
+
+num_words = {
+    'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5',
+    'six': '6', 'seven': '7', 'eight': '8', 'nine': '9', 'ten': '10',
+    'eleven': '11', 'twelve': '12', 'thirteen': '13', 'fourteen': '14',
+    'fifteen': '15', 'sixteen': '16', 'seventeen': '17', 'eighteen': '18',
+    'nineteen': '19', 'twenty': '20', 'thirty': '30', 'forty': '40',
+    'fifty': '50', 'sixty': '60', 'seventy': '70', 'eighty': '80', 'ninety': '90',
+    'hundred': '100', 'thousand': '1000', 'million': '1000000', 'billion': '1000000000'
+}
+
+def convert_number_words(text):
+    #Replacing written numbers for literal ones
+    for word, digit in num_words.items():
+        text = re.sub(r'\b' + word + r'\b', digit, text)
+    return text
