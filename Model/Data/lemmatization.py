ecfa4e700ee7ba128576ff197844bf735529cc58
Santiago Becerra <santialmancy@gmail.com>
2024-06-16 13:49:06 -0400

    lemmatization&StopWordsRemoval - Finding tags for lemmatization

---
+++ Model/Data/lemmatization.py
@@ -0,0 +1,16 @@
+import nltk
+from nltk.corpus import wordnet
+nltk.download('wordnet')
+
+# Pos Tag Finder function
+def pos_tag_finder(nltk_tag):
+    if nltk_tag.startswith('J'):
+        return wordnet.ADJ
+    elif nltk_tag.startswith('V'):
+        return wordnet.VERB
+    elif nltk_tag.startswith('N'):
+        return wordnet.NOUN
+    elif nltk_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return None
