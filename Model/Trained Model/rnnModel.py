4a03f7586881e388e8d820da25f85540f1d2ec81
Sebastian Itamari <sebaton66@gmail.com>
2024-06-20 11:29:01 -0400

    rnnModel - Creating data and labels

--- Model/Trained Model/rnnModel.py
+++ Model/Trained Model/rnnModel.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import keras
 
 if __name__ == "__main__":
     # Importing the data
@@ -7,11 +8,20 @@ if __name__ == "__main__":
     df = pd.read_csv(dataPath)
 
     # Number of samples per group
-    n_samples = 1000
-    random_state = 42  # Set a seed for reproducibility
+    nSamples = 1000
+    randomState = 42  # Set a seed for reproducibility
 
     # Select 1000 samples of 'text' for each unique value in 'overall'
-    df = df.groupby('overall', group_keys=False).apply(lambda x: x.sample(min(len(x), n_samples), random_state=random_state))
+    df = df.groupby('overall', group_keys=False).apply(lambda x: x.sample(min(len(x), nSamples), random_state=randomState))
+    
+    # Shuffle the data to remove grouping by 'overall'
+    df = df.sample(frac=1, random_state=randomState).reset_index(drop=True)
 
     #overall_counts = df['overall'].value_counts()
     #print(overall_counts)
+    #print(df)
+
+    # Creating data and its labels to training
+    dataX = df['text']
+    dataY = keras.utils.to_categorical(df['overall'] - 1, 5) # Converting to one-hot vector to classify the data
+    #print(dataY)
