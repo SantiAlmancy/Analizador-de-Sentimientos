6d8c513b23000a7f43d7f557b2c0e4893c816d4f
Santiago Becerra <santialmancy@gmail.com>
2024-06-21 18:49:28 -0400

    huggingFaceTransformer - Preprocessing csv for hugging face

--- Model/Trained Model/huggingFaceModel/dataManager.py
+++ Model/Trained Model/huggingFaceModel/dataManager.py
@@ -12,3 +12,14 @@ class DataManager:
             print(f"Error reading data: {e}")
             return None
 
+    def preprocess_data(self, df):
+        try:
+            df = df[['text', 'overall']]
+            df = df.copy()
+            df.rename(columns={'overall': 'label'}, inplace=True)
+            df = df.dropna(subset=['label'])
+            df['label'] = df['label'].astype(int) - 1
+            return df
+        except Exception as e:
+            print(f"Error preprocessing data: {e}")
+            return None
