f921c954b9660f0b31e54440f9e355646e169b67
mathpro28 <matemerc@gmail.com>
2024-06-20 20:59:40 -0400

    us-wp-back Remove the rating field for the POST

--- Web Page/backend/reviews/serializers.py
+++ Web Page/backend/reviews/serializers.py
@@ -7,5 +7,5 @@ class HotelSerializer(serializers.Serializer):
 class ReviewSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)  # Make id read-only
     hotel_id = serializers.IntegerField()
-    rating = serializers.IntegerField(min_value=1, max_value=5)
+    # rating = serializers.IntegerField(min_value=1, max_value=5)
     comment = serializers.CharField(max_length=1024)
