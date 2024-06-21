469bbb3810346fc5628e20d8575be44be8ea7177
Santiago Becerra <santialmancy@gmail.com>
2024-06-21 18:53:06 -0400

    huggingFaceTransformer - Defining training arguments for model

--- Model/Trained Model/huggingFaceModel/modelTrainer.py
+++ Model/Trained Model/huggingFaceModel/modelTrainer.py
@@ -2,7 +2,6 @@ from transformers import AutoModelForSequenceClassification, Trainer, TrainingAr
 from sklearn.metrics import accuracy_score, f1_score
 import torch
 
-# Clase para manejar el entrenamiento del modelo
 class ModelTrainer:
     def __init__(self, model_checkpoint, train_dataset, test_dataset, tokenizer):
         self.model_checkpoint = model_checkpoint
@@ -14,6 +13,29 @@ class ModelTrainer:
             print(f"Using CUDA with GPU: {torch.cuda.get_device_name(0)}")
         else:
             print("CUDA is not available. Training on CPU...")
+
+    def train_model(self, output_dir, num_train_epochs=3, learning_rate=2e-5, batch_size=16):
+        try:
+            model = AutoModelForSequenceClassification.from_pretrained(self.model_checkpoint, num_labels=5)
+            model.to(self.device)
+            
+            training_args = TrainingArguments(
+                output_dir=output_dir,
+                num_train_epochs=num_train_epochs,
+                learning_rate=learning_rate,
+                per_device_train_batch_size=batch_size,
+                per_device_eval_batch_size=batch_size,
+                weight_decay=0.01,
+                eval_strategy="epoch",
+                disable_tqdm=False,
+                push_to_hub=True,
+                report_to="none"
+            )
+
+            
+        except Exception as e:
+            print(f"Error training model: {e}")
+            return None
 
     def compute_metrics(self, pred):
         labels = pred.label_ids
