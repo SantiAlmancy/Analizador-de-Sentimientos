de48ec5e022f8163238e95786cb179385d7ed73c
Sebastian Itamari <sebaton66@gmail.com>
2024-06-21 17:12:14 -0400

    dataDistribution - Adding the generation of csv file with two categories

--- Model/Data/csvGeneration.py
+++ Model/Data/csvGeneration.py
@@ -25,7 +25,8 @@ def showDataDistribution(datafrane,columnName,title):
     print(counts)
 
     # Defining a predefined color palette (in this case tab10)
-    colors = plt.get_cmap('tab10').colors[:len(counts)]
+    cmap = plt.get_cmap('tab10')
+    colors = [cmap(i) for i in range(len(counts))]
 
     # Creating bar chart
     plt.figure(figsize=(10, 6))
@@ -83,12 +84,45 @@ def distributeDataThree(dataframe):
     dfPos = dfPos.head(minCount)
 
     # Change the values of 'overall' to the new categories
-    dfNeg['overall'] = 1
-    dfNeu['overall'] = 2
-    dfPos['overall'] = 3
+    dfNeg['overall'] = 1.0
+    dfNeu['overall'] = 2.0
+    dfPos['overall'] = 3.0
 
     # Combine the three dataframes
     combinedDf = pd.concat([dfNeg, dfNeu, dfPos])
+
+    # Shuffle the dataframe
+    shuffledDf = combinedDf.sample(frac=1).reset_index(drop=True)
+    
+    return shuffledDf
+
+def distributeDataTwo(dataframe):
+    # Filter out records where 'overall' is 0
+    filteredData = dataframe[dataframe['overall'] != 0]
+
+    # Create a dataframe for each category
+    dfNeg = filteredData[filteredData['overall'] < 3]
+    dfPos = filteredData[filteredData['overall'] > 3]
+
+    # Sort each dataframe in descending order based on 'num_helpful_votes'
+    dfNeg = dfNeg.sort_values(by='num_helpful_votes', ascending=False)
+    dfPos = dfPos.sort_values(by='num_helpful_votes', ascending=False)
+
+    # Get the number of rows in each dataframe
+    countNeg = dfNeg.shape[0]
+    countPos = dfPos.shape[0]
+    minCount = min(countNeg, countPos)
+
+    # Take the first minCount rows from each dataframe
+    dfNeg = dfNeg.head(minCount)
+    dfPos = dfPos.head(minCount)
+
+    # Change the values of 'overall' to the new categories
+    dfNeg['overall'] = 1.0
+    dfPos['overall'] = 2.0
+
+    # Combine the three dataframes
+    combinedDf = pd.concat([dfNeg, dfPos])
 
     # Shuffle the dataframe
     shuffledDf = combinedDf.sample(frac=1).reset_index(drop=True)
@@ -107,44 +141,44 @@ if __name__ == "__main__":
     # Adding a column 'language' with the language of the text in the column 'text'
     data['language'] = data['text'].apply(detectLanguage)
 
-    # Filtering the language of data
+    # Filter the language of data
     data = data[data['language'] == 'en']
 
-    # Adding a column 'overall' with the extracted value from the column 'ratings'.
+    # Add a column 'overall' with the extracted value from the column 'ratings'
     data['overall'] = data['ratings'].apply(extractOverall)
 
-    # Adding a column 'word_count' with the word count of the text in the column 'text'
+    # Add a column 'word_count' with the word count of the text in the column 'text'
     data['word_count'] = data['text'].apply(countWords)
 
-    # Filtering out rows with more than 250 words
-    data = data[data['word_count'] <= 350]
+    # Filter out rows with more than 250 words
+    data = data[data['word_count'] <= 250]
 
-    # Selecting important columns
+    # Select important columns
     data = data[['title', 'text', 'offering_id', 'num_helpful_votes', 'overall']]
     print(data)
 
-    # Showing the initial data distribution after the language filtering
+    # Show the initial data distribution after the language filtering
     showDataDistribution(data,'overall','Initial data distribution')
 
-    data = distributeDataFive(data)
+    data = distributeDataThree(data)
     
     print(data)
 
-    # Showing the new data distribution after the distribution of data
+    # Show the new data distribution after the distribution of data
     showDataDistribution(data,'overall','New data distribution')  
 
-    # Importing hotels data
+    # Import hotels data
     pathHotelData = os.getenv('HOTEL_DATA_PATH')
     hotelData = pd.read_csv(pathHotelData)
     hotelData = hotelData[['id', 'hotel_class', 'name', 'address']]
 
-    # Merging filterdData and hotelData
+    # Merge filterdData and hotelData
     data = data.merge(hotelData, left_on='offering_id', right_on='id')
 
-    # Replacing None values in the column 'hotel_class' with 0
+    # Replace None values in the column 'hotel_class' with 0
     data['hotel_class'] = data['hotel_class'].fillna(0)
 
-    # Selecting important columns
+    # Select important columns
     data = data[['title', 'text', 'overall', 'num_helpful_votes', 'name','hotel_class', 'address']]
     print(data)
 
