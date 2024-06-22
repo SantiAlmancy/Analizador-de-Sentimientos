60037d4887a7d959b45887c3ecc5d0835fb72e62
Sebastian Itamari <sebaton66@gmail.com>
2024-06-22 10:12:32 -0400

    us-wp-front - Creating the routes to the pages

--- Web Page/frontend/hotel-reviews/src/App.js
+++ Web Page/frontend/hotel-reviews/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Hotels from './pages/Hotels';
+import Reviews from './pages/Reviews';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Hotels/>}/>
+        <Route path="/reviews" element={<Reviews/>}/>
+      </Routes>
     </div>
   );
 }
