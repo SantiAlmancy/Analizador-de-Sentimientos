398e45df96b0911bb681c07dbddac5e5049ceeb2
SantiAlmancy <santialmancy@gmail.com>
2024-06-23 18:57:35 -0400

    modelsAndRefactor - Creating urls for endpoints

--- Web Page/backend/reviews/urls.py
+++ Web Page/backend/reviews/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import HotelListView, HotelReviewListView, CreateReviewView, CreateHotelView, TextClassificationView
+from .views import HotelListView, HotelReviewListView, CreateReviewView, CreateHotelView, HuggingFacePredictionView, KerasModelPredictionView
 
 urlpatterns = [
     path('hotels/', HotelListView.as_view(), name='hotel-list'),
     path('hotels/<str:hotel_id>/reviews/', HotelReviewListView.as_view(), name='hotel-review-list'),
     path('reviews/', CreateReviewView.as_view(), name='create-review'),
     path('hotels/add/', CreateHotelView.as_view(), name='create-hotel'),  # New endpoint for adding hotels
-    path('classify-text/', TextClassificationView.as_view(), name='classify-text'),
+    path('transformer-predict/', HuggingFacePredictionView.as_view(), name='transformer-model-predict'),
+    path('keras-predict/', KerasModelPredictionView.as_view(), name='keras-model-predict'),
 ]
