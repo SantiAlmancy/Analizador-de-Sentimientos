72e63807fa79c9ec8efbdb9ced0b1a0252a2ff98
SantiAlmancy <santialmancy@gmail.com>
2024-06-24 20:57:47 -0400

    modelsAndRefactor - Fixing predictions

--- Web Page/backend/reviews/predictionModels.py
+++ Web Page/backend/reviews/predictionModels.py
@@ -23,29 +23,36 @@ class Model:
         self.model = load_model(r'C:\Users\PC\Documents\Dank\Model2Categories')
 
         # Load the transformer model
-        self.classifier = pipeline("text-classification", model="Almancy/finetuning-emotion-model-5")
+        self.classifier = pipeline("text-classification", model="Almancy/finetuning-emotion-model")
         
         # Initialize other parameters if needed
         self.maxLen = 250  # Maximum sequence length expected by the model
+
+        self.wordTokenizer = self.createTokenizer()
 
     def preprocess_text(self, text, isTensor=False):
         text = modelInputPreprocess.preprocessTextInput(text)
         print(text)
         if (isTensor):
             # Tokenize and pad the input text
-            tokenizer = Tokenizer()
-            tokenizer.fit_on_texts([text])
-            sequence = tokenizer.texts_to_sequences([text])
+            sequence = self.wordTokenizer.texts_to_sequences([text])
             padded_sequence = pad_sequences(sequence, padding='post', maxlen=self.maxLen)
             
             return padded_sequence
         else:
             return text
         
+    def createTokenizer(self):
+        df = pd.read_csv(r'C:\Users\PC\Documents\Dank\preprocessedData2.csv')
+        dataX = df['text']
+        wordTokenizer = Tokenizer()
+        wordTokenizer.fit_on_texts(dataX)
+        return wordTokenizer
+        
     def mapLabels(self, prediction):
         label_map = {
-                "LABEL_0": "negative",
-                "LABEL_1": "very_negative",
+                "LABEL_0": "very_negative",
+                "LABEL_1": "negative",
                 "LABEL_2": "neutral",
                 "LABEL_3": "positive",
                 "LABEL_4": "very_positive"
@@ -64,9 +71,9 @@ class Model:
         prediction = self.model.predict(processed_text)
         
         # Convert prediction to a category or class label
-        predicted_category = np.argmax(prediction, axis=-1)
+        predicted_category = prediction.argmax(axis=-1)
 
-        # Assuming binary classification: 0 - Negative, 1 - Positive
-        predicted_label = "Positive" if predicted_category[0] == 1 else "Negative"
+        category_map = {0: 'negative', 1: 'positive'}
+        predicted_label = category_map[predicted_category[0]]
 
         return predicted_label
